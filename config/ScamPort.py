e452038ac9b784f37d2e344e4327f1bbfd2a61cf
@Slayerkkk_ <110862456+slayerkk@users.noreply.github.com>
2023-03-06 01:59:08 -0300

    Add files via upload

---
+++ config/ScamPort.py
@@ -0,0 +1,20 @@
+import socket, time
+
+verde = '\033[0;30;32m'
+verde1 = '\033[0;30;42m'
+nulo = '\033[0;30;0m'
+vermelho = '\033[0;30;31m'
+vermelho1 = '\033[0;30;41m'
+print('Ex: youtube.com')
+a = input('site:')
+
+Portas = [22, 23, 25, 53, 8080, 80, 443, 110, 143, 177]
+
+for Portas in Portas:
+    cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    cliente.settimeout(0.1)
+    code = cliente.connect_ex((f'{a}', Portas))
+    if code == 0:
+        print(f'{verde1}{Portas} OPEN{nulo}')
+    if code == 10035:
+        print(f'{vermelho1}{Portas} CLOSED{nulo}')
---
+++ config/todasPorts.py
@@ -0,0 +1,27 @@
+#65.535
+import socket, time
+
+verde = '\033[0;30;32m'
+verde1 = '\033[0;30;42m'
+nulo = '\033[0;30;0m'
+vermelho = '\033[0;30;31m'
+vermelho1 = '\033[0;30;41m'
+print('Ex: youtube.com')
+a = input('site:')
+
+Portas = 1
+
+while True:
+    Portas = Portas + 1
+
+
+    cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    cliente.settimeout(0.1)
+    code = cliente.connect_ex((f'{a}', Portas))
+    if code == 0:
+        input(f'''{verde1}{Portas} OPEN{nulo}
+Aperte enter para continuar...''')
+    if code == 10035:
+        print(f'{vermelho1}{Portas} CLOSED{nulo}')
+        if Portas == 65.535:
+            break
